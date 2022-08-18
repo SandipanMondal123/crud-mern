1d0220165b666b25faaf5fae159e36d1be2e70c0
Sandipan Mondal <office.smondal@gmail.com>
2022-08-18 19:36:45 -0400

    .

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -3,12 +3,21 @@ import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-    user: null ? user : null,
+    user: user ? user : null,
     isError: false,
     isSuccess: false,
     isLoading: false,
     message: ''
 }
+
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => 
+{
+    try {
+        return await authService.register(user)
+    } catch (error){
+
+    }
+})
 
 export const authSlice = createSlice({
     name: 'auth',
